6e4a5177f935a2e524787f832d4b0e65e4acf581
EC2 Default User <ec2-user@ip-172-31-30-93.ec2.internal>
2022-03-10 06:46:46 +0000

    Test1

---
+++ index.js
@@ -0,0 +1,26 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from your CS301 team!'),
+    };
+    return response;
+};
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from your CS301 team!'),
+    };
+    return response;
+};
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from your CS301 team!'),
+    };
+    return response;
+};
+
